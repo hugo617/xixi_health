31c5731a101e2f1080abce27191fe30cbf459402
star <star@starworking.com>
2025-11-24 21:49:36 +0800

    完成侧边栏的视图渲染

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :show_sidenav?  
+  def show_sidenav?
+  # 只在特定的控制器和动作中显示侧边栏
+  (controller_name == 'users' && action_name == 'index') ||
+  (controller_name == 'reports' && action_name.in?(%w[index]))
+  end
+  
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
